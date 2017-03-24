693dfab62f96fc3d4cefad52fd020a953b9f9f2a
James Duncan <james.a.duncan91@gmail.com>
2017-03-24 11:05:35 +0000

    created CRUD functions for ticket, tested on PRY

--- seeds.rb
+++ seeds.rb
@@ -4,9 +4,9 @@ require_relative( 'models/ticket' )
 
 require( 'pry-byebug' )
 
-# Ticket.delete_all()
+Ticket.delete_all()
 Customer.delete_all()
-# Film.delete_all()
+Film.delete_all()
 
 customer1 = Customer.new({ 'name' => 'James', 'funds' => 25.00})
 customer1.save()
@@ -14,6 +14,9 @@ customer1.save()
 film1 = Film.new({'title' => 'Die Hard', 'price' => 10.00})
 
 film1.save()
+
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket1.save()
 
 binding.pry
 nil
